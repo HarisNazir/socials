8c48352a68614417c2b40ecfbb90e54c2f70c73c
Haris Nazir <haris.nazir1963@gmail.com>
2020-11-28 22:03:48 +0000

    Added URLS and Model View

--- socials/api/views.py
+++ socials/api/views.py
@@ -5,7 +5,7 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     
